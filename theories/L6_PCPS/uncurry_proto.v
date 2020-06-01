027fbcc69676be1dffbb801dd9977bf112677692
John Li <johnli0135@gmail.com>
2020-06-01 16:29:11 -0400

    The other half of uncurrying corresp proof

--- theories/L6_PCPS/uncurry_proto.v
+++ theories/L6_PCPS/uncurry_proto.v
@@ -43,6 +43,12 @@ Definition fresher_than (x : cps.var) (S : Ensemble cps.var) : Prop :=
 Lemma fresher_than_antimon x S1 S2 : S1 \subset S2 -> fresher_than x S2 -> fresher_than x S1.
 Proof. intros HS12 Hfresh y Hy; apply Hfresh, HS12, Hy. Qed.
 
+Lemma fresher_than_monotonic x y S : (y > x -> fresher_than x S -> fresher_than y S)%positive.
+Proof. intros Hxy Hfresh z Hz. assert (x > z)%positive by now apply Hfresh. lia. Qed.
+
+Lemma fresher_than_Union x S1 S2 : fresher_than x S1 -> fresher_than x S2 -> fresher_than x (S1 :|: S2).
+Proof. intros HS1 HS2 y Hy; destruct Hy as [y Hy|y Hy]; auto. Qed.
+
 Definition S {A} (C : exp_c A exp_univ_exp) (e : univD A) : Set :=
   {x | fresher_than x (used_vars ![C ⟦ e ⟧])}.
 
@@ -151,6 +157,11 @@ Proof.
   specialize (IHxs H); lia.
 Qed.
 
+Corollary gensyms_increasing' {A} :
+  forall x (xs : list A) x' xs', (x', xs') = gensyms x xs -> 
+  forall y, List.In y xs' -> (![y] >= x)%positive.
+Proof. mk_corollary @gensyms_increasing. Qed.
+
 Lemma gensyms_upper {A} x (xs : list A) :
   (fst (gensyms x xs) >= x)%positive /\
   forall y, List.In y (snd (gensyms x xs)) -> (fst (gensyms x xs) > ![y])%positive.
@@ -167,6 +178,14 @@ Proof.
     destruct Hy as [Hy|Hy]; [inversion Hy; simpl; lia|].
     now specialize (IHxsy (mk_var y) Hy).
 Qed.
+
+Corollary gensyms_upper1 {A} : forall x (xs : list A) x' xs', (x', xs') = gensyms x xs -> (x' >= x)%positive.
+Proof. mk_corollary @gensyms_upper. Qed.
+
+Corollary gensyms_upper2 {A} :
+  forall x (xs : list A) x' xs', (x', xs') = gensyms x xs ->
+  forall y, List.In y xs' -> (x' > ![y])%positive.
+Proof. mk_corollary @gensyms_upper. Qed.
 
 Lemma gensyms_NoDup {A} x (xs : list A) : NoDup (snd (gensyms x xs)).
 Proof.
@@ -186,6 +205,60 @@ Qed.
 
 Corollary gensyms_NoDup' {A} : forall x (xs : list A) x' xs', (x', xs') = gensyms x xs -> NoDup xs'.
 Proof. mk_corollary @gensyms_NoDup. Qed.
+
+Lemma gensyms_fresher_than {A} (xs : list A) :
+  forall x y S x' xs',
+  fresher_than x S ->
+  (y >= x)%positive ->
+  (x', xs') = gensyms y xs ->
+  Disjoint _ S (FromList ![xs']).
+Proof.
+  induction xs.
+  - simpl; intros x y S x' xs' Hfresh Hyx Hgen; inversion Hgen; subst.
+    simpl; normalize_sets; eauto with Ensembles_DB.
+  - unfold gensyms; fold @gensyms; intros x y S x' xs' Hfresh Hyx Hgen.
+    destruct (gensyms (1 + y)%positive xs) as [x'' xs''] eqn:Hxs.
+    inversion Hgen; subst; simpl; normalize_sets.
+    apply Union_Disjoint_r; [|eapply (IHxs (1 + y)%positive); eauto; try lia].
+    + unfold fresher_than in Hfresh.
+      constructor; intros arb; intros HSx; unfold Ensembles.In in HSx.
+      destruct HSx as [arb HS Hx]; inversion Hx; subst.
+      specialize (Hfresh _ HS); lia.
+    + eapply fresher_than_monotonic; eauto; lia.
+Qed.
+
+Local Ltac show_Disjoint arb Harbx Harby :=
+  let Harb := fresh "Harb" in
+  constructor; intros arb Harb; unfold Ensembles.In in Harb;
+  destruct Harb as [arb Harbx Harby].
+
+Lemma gensyms_disjoint {A B} (xs : list A) (ys : list B) x0 x1 x2 xs' ys' :
+  (x1, xs') = gensyms x0 xs ->
+  (x2, ys') = gensyms x1 ys ->
+  Disjoint _ (FromList ![xs']) (FromList ![ys']).
+Proof.
+  intros Hxs' Hys'; show_Disjoint arb Harbx Harby.
+  unfold Ensembles.In, FromList in Harbx, Harby.
+  apply (in_map mk_var) in Harbx; apply (in_map mk_var) in Harby.
+  simpl in Harbx, Harby; normalize_roundtrips.
+  assert (x1 > ![mk_var arb])%positive by (eapply gensyms_upper2; eassumption); simpl in *.
+  assert (![mk_var arb] >= x1)%positive by (eapply gensyms_increasing'; eassumption); simpl in *.
+  lia.
+Qed.
+
+Lemma gensyms_list_fresher {A} x y (ys : list A) y' ys' S :
+  fresher_than x S ->
+  (y >= x)%positive ->
+  (y', ys') = gensyms y ys ->
+  Disjoint _ S (FromList ![ys']).
+Proof.
+  intros Hfresh Hyx Hgen; show_Disjoint arb Harbx Harby.
+  unfold Ensembles.In, FromList in Harby.
+  apply (in_map mk_var) in Harby; simpl in Harby; normalize_roundtrips.
+  assert (![mk_var arb] >= y)%positive by (eapply gensyms_increasing'; eauto); simpl in *.
+  assert (x > arb)%positive by now apply Hfresh.
+  lia.
+Qed.
 
 Lemma bool_true_false b : b = false -> b <> true. Proof. now destruct b. Qed.
 
@@ -228,16 +301,51 @@ Proof.
     clearpose Hxgv1 xgv1 (gensyms next_x gv); destruct xgv1 as [next_x0 gv1].
     clearpose Hxfv1 xfv1 (gensyms next_x0 fv); destruct xfv1 as [next_x1 fv1].
     clearpose Hf1 f1 next_x1.
-    (* Prove that all the above code actually satisfies the side conditions *)
+    (* Prove that all the above code actually satisfies the side condition *)
+    assert (NoDup gv1) by (eapply gensyms_NoDup'; eassumption).
+    assert (NoDup fv1) by (eapply gensyms_NoDup'; eassumption).
+    assert (length gv1 = length gv) by (eapply gensyms_len'; eassumption).
+    assert (length fv1 = length fv) by (eapply gensyms_len'; eassumption).
+    assert (next_x0 >= next_x)%positive by (eapply gensyms_upper1; eassumption).
+    assert (next_x1 >= next_x0)%positive by (eapply gensyms_upper1; eassumption).
+    assert (Disjoint _ (FromList ![gv1]) (FromList ![fv1])) by (eapply gensyms_disjoint; eassumption).
     eapply success with (f1 := mk_var f1) (fv1 := fv1) (gv1 := gv1) (next_x := (next_x1 + 1)%positive);
-      repeat match goal with |- _ /\ _ => split | |- fresh_copies _ _ => split end; try solve
-      [reflexivity|assumption|subst;reflexivity
-      |eapply gensyms_NoDup'; eassumption
-      |eapply gensyms_len'; eassumption].
-    + admit.
-    + admit.
-    + admit.
-    + admit.
+      repeat match goal with
+      | |- _ /\ _ => split
+      | |- fresh_copies _ _ => split
+      | |- Disjoint _ (_ :|: _) _ => apply Union_Disjoint_l
+      end;
+      try solve [reflexivity|assumption|subst;reflexivity].
+    + eapply gensyms_list_fresher; [| |eassumption]; try eassumption; lia.
+    + eapply gensyms_list_fresher; [| |eassumption]; try eassumption; lia.
+    + match goal with |- ~ _ \in _ => rewrite <- Disjoint_Singleton_In; [|auto with Decidable_DB] end.
+      repeat match goal with |- Disjoint _ (_ :|: _) _ => apply Union_Disjoint_l end.
+      * subst f1; simpl; show_Disjoint arb Harbx Harby.
+        assert (next_x > arb)%positive by now apply Hnext_x.
+        inversion Harby; lia.
+      * show_Disjoint arb Harbx Harby; simpl in Harby.
+        unfold Ensembles.In, FromList in Harbx; apply (in_map mk_var) in Harbx.
+        simpl in Harbx; normalize_roundtrips.
+        assert (Harb0 : (next_x0 > ![mk_var arb])%positive) by (eapply gensyms_upper2; eauto).
+        simpl in Harb0; inv Harby.
+        lia. (* arb >= next_0 >= next_x > arb *)
+      * show_Disjoint arb Harbx Harby.
+        simpl in Harby; inv Harby.
+        unfold Ensembles.In, FromList in Harbx; apply (in_map mk_var) in Harbx.
+        simpl in Harbx; normalize_roundtrips.
+        assert (Harb : (arb > ![mk_var arb])%positive) by (eapply gensyms_upper2; eauto).
+        simpl in Harb; lia.
+    + repeat match goal with |- fresher_than _ (_ :|: _) => apply fresher_than_Union end.
+      * eapply fresher_than_monotonic; eauto; lia.
+      * intros z Hz; unfold Ensembles.In, FromList in Hz; apply (in_map mk_var) in Hz.
+        simpl in Hz; normalize_roundtrips.
+        assert (Hz0 : (next_x0 > ![mk_var z])%positive) by (eapply gensyms_upper2; eauto).
+        simpl in Hz0; lia.
+      * intros z Hz; unfold Ensembles.In, FromList in Hz; apply (in_map mk_var) in Hz.
+        simpl in Hz; normalize_roundtrips.
+        assert (Hz0 : (next_x1 > ![mk_var z])%positive) by (eapply gensyms_upper2; eauto).
+        simpl in Hz0; lia.
+      * simpl; intros f1' Hf1'; inversion Hf1'; lia.
     + left; exact I. (* TODO replace with actual proof *)
     + destruct (Maps.PTree.get _ _) as [[|]|] eqn:Hget'; [reflexivity|inversion Huncurried..].
   (* Obligation 2 of 2: explain how to maintain fresh name invariant across edit *)
@@ -254,4 +362,4 @@ Proof.
     do 10 normalize_used_vars'; repeat normalize_sets.
     repeat rewrite strip_vars_app; repeat normalize_sets.
     intros arbitrary; repeat rewrite In_or_Iff_Union; tauto.
-Admitted.
+Defined.
