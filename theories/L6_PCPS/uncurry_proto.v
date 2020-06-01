9eb40f16fcef09f76cfb800bcde60a379dd3d8db
John Li <johnli0135@gmail.com>
2020-05-31 23:32:43 -0400

    Add fun_tag generation + inlining heuristc update to skeleton

--- theories/L6_PCPS/uncurry_proto.v
+++ theories/L6_PCPS/uncurry_proto.v
@@ -2,7 +2,6 @@
 
 Require Import Coq.Strings.String.
 Require Import Coq.NArith.BinNat Coq.PArith.BinPos Coq.Sets.Ensembles Omega.
-Require L6.cps. (* for module M *)
 Require Import L6.Prototype.
 Require Import L6.cps_proto.
 Require Import identifiers.  (* for max_var, occurs_in_exp, .. *)
@@ -12,8 +11,26 @@ Require Import L6.Ensembles_util L6.List_util L6.cps_util L6.state.
 Require Import Coq.Lists.List.
 Import ListNotations.
 
-Definition miscR : Set := unit.
-Definition miscS : Set := PM bool * bool.
+Definition R_misc : Set := unit.
+
+(* pair of 
+   1 - max number of arguments 
+   2 - encoding of inlining decision for beta-contraction phase *)
+Definition St : Set := (nat * (PM nat))%type.
+(* 0 -> Do not inline, 1 -> uncurried function, 2 -> continuation of uncurried function *)
+
+(* Maps (arity+1) to the right fun_tag *)
+Definition arity_map : Set := PM fun_tag.
+Definition local_map : Set := PM bool.
+ 
+(* The state for this includes 
+   1 - a boolean for tracking whether or not a reduction happens
+   2 - Map recording the (new) fun_tag associated to each arity
+   3 - local map from var to if function has already been uncurried
+   4 - Map for uncurried functions for a version of inlining
+*)
+Definition S_misc : Set := (bool * arity_map * local_map * St). 
+(* TODO: comp_data for updating arity_map. Need to make comp_data be in Set *)
 
 Definition delay_t {A} (e : univD A) : Set := unit.
 
@@ -55,7 +72,8 @@ Inductive uncurry_step : exp -> exp -> Prop :=
   forall (C : frames_t exp_univ_fundefs exp_univ_exp)
     (f f1 : var) (ft ft1 : fun_tag) (k k' : var) (kt : fun_tag) (fv fv1 : list var)
     (g g' : var) (gt : fun_tag) (gv gv1 : list var) (ge : exp) (fds : fundefs)
-    (lhs rhs : fundefs) (s : Ensemble cps.var) (next_x : cps.var),
+    (lhs rhs : fundefs) (s : Ensemble cps.var) (next_x : cps.var)
+    (fp_numargs : nat) (ms : S_misc),
   (* Non-linear LHS constraints *)
   k = k' /\
   g = g' /\
@@ -73,24 +91,34 @@ Inductive uncurry_step : exp -> exp -> Prop :=
   ~ ![f1] \in (s :|: FromList ![gv1] :|: FromList ![fv1]) /\
   (* (3) next_x must be a fresh variable *)
   fresher_than next_x (s :|: FromList ![gv1] :|: FromList ![fv1]) /\
-  True (* - TODO: fun_tag stuff *) ->
+  (* (4) ft1 is the appropriate fun_tag and ms is an updated misc state. *)
+  fp_numargs = length fv + length gv /\
+  (let '(_, aenv, _, _) := ms in PM.get ![ft1] aenv <> None) -> (* TODO: proper side condition *)
   (* 'Irrelevant' guard *)
-  When (fun (r : miscR) (s : miscS) =>
+  When (fun (r : R_misc) (s : S_misc) =>
+    let '(b, aenv, lm, s) := s in
     (* g must not have been already uncurried by an earlier pass *)
-    match cps.M.get ![g] (fst s) with
+    match cps.M.get ![g] lm with
     | Some true => true
     | _ => false
     end) ->
   (* The rewrite *)
   uncurry_step
     (C ⟦ Fcons f ft (k :: fv) (Efun (Fcons g gt gv ge Fnil) (Eapp k' kt [g'])) fds ⟧)
-    (C ⟦ (* Mark g as uncurried and set a flag (used to iterate to fixed point) *)
-         Modify (fun '(s, _) => (PM.set (un_var g) true s, true))
+    (C ⟦ Put (
+           let '(b, aenv, lm, s) := ms in
+           (* Set flag to indicate that a rewrite was performed (used to iterate to fixed point) *)
+           let b := true in
+           (* Mark g as uncurried *)
+           let lm := PM.set ![g] true lm in
+           (* Update inlining heuristic so inliner knows to inline fully saturated calls to f *)
+           let s := (max (fst s) fp_numargs, (PM.set ![f] 1 (PM.set ![g] 2 (snd s)))) in
+           (true, aenv, lm, s) : S_misc)
          (* Rewrite f as a wrapper around the uncurried f1 *)
          (Fcons f ft (k :: fv1) (Efun (Fcons g gt gv1 (Eapp f1 ft1 (gv1 ++ fv1)) Fnil) (Eapp k kt [g]))
          (Fcons f1 ft1 (gv ++ fv) ge (Rec fds))) ⟧).
 
-Definition rw_cp : rewriter exp_univ_exp uncurry_step miscR miscS (@delay_t) (@R_C) (@R_e) (@S).
+Definition rw_cp : rewriter exp_univ_exp uncurry_step R_misc S_misc (@delay_t) (@R_C) (@R_e) (@S).
 Proof.
   mk_rw;
     (* This particular rewriter's delayed computation is just the identity function,
@@ -110,12 +138,17 @@ Proof.
     destruct (eq_var g g') eqn:Hgg'; [|exact failure].
     rewrite Pos.eqb_eq in Hkk', Hgg'.
     (* Check whether g has already been uncurried before *)
-    destruct (Maps.PTree.get ![mk_var g] (fst ms)) as [[|]|]; [|exact failure..].
+    destruct ms as [[[b aenv] lm] heuristic].
+    destruct (PM.get g lm) as [[|]|] eqn:Huncurried; [|exact failure..].
     (* Check that {g, k} ∩ vars(ge) = ∅ *)
-    idtac "TODO: occurs_in_exp".
-    (* Generate f1, ft1, fv1, gv1, next_x *)
+    destruct (occurs_in_exp g ![ge]) eqn:Hocc_g; [|exact failure]. (* TODO: avoid the conversion *)
+    destruct (occurs_in_exp k ![ge]) eqn:Hocc_k; [|exact failure]. (* TODO: avoid the conversion *)
+    rewrite occurs_in_exp_iff_used_vars in Hocc_g, Hocc_k.
+    (* Generate ft1 + new misc state ms *)
+    idtac "TODO: generate ft1 and new misc state ms".
+    (* Generate f1, fv1, gv1, next_x *)
     idtac "TODO: generate f1, ft1, fv1, gv1, next_x".
-    (* Prove that these satisfy the proper preconditions *)
+    (* Prove that the freshly generated names are actually fresh *)
     idtac "TODO: vars proof".
     admit.
   (* Obligation 2 of 2: explain how to maintain fresh name invariant across edit *)
@@ -123,7 +156,7 @@ Proof.
     (* Env is easy, because the uncurryer doesn't need it *)
     split; [exact tt|].
     (* For state, need create a fresh variable. Thankfully, we have one: next_x *)
-    exists next_x; unfold Modify, Rec.
+    exists next_x; unfold Put, Rec.
     repeat match goal with H : _ /\ _ |- _ => destruct H end.
     eapply fresher_than_antimon; [|eassumption].
     admit.
